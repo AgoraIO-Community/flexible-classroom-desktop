ec01f39a2512477be86cf837e7eecf4e6f1a2320
zhanheng <zhanheng@agora.io>
2022-11-17 17:47:07 +0800

    chore: ignore lockfile

--- scripts/install-apaas-modules.sh
+++ scripts/install-apaas-modules.sh
@@ -3,7 +3,7 @@
 #git submodule add ssh://git@git.agoralab.co/aduc/apaas-common-libs-web.git packages/agora-common-libs
 git submodule update --init --recursive
 
-yarn install
+yarn install —no-lockfile
 
 bin_path=node_modules/.bin
 
