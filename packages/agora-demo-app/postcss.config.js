4b49d8f3396e6e81aa27b402d6db5caeea6f9858
任一飞 <renyifei@agora.io>
2023-02-14 07:12:49 +0000

    Merge pull request #75 in ADUC/open-flexible-classroom-desktop from feat/big-class-h5 to release/2.8.20
    Squashed commit of the following:
    
    commit d026f9d8ef52257bd8252f8701aae8f14ff7b731
    Author: zhanheng <zhanheng@agora.io>
    Date:   Tue Feb 14 14:04:54 2023 +0800
    
        fix: update proctor sdk version
    
    commit 74ca8f6ec75ff433ac90e64cd8a88a5051cda380
    Merge: 5b4b375 08ddadd
    Author: zhanheng <zhanheng@agora.io>
    Date:   Tue Feb 14 14:03:53 2023 +0800
    
        fix: resolve conflicts
    
    commit 5b4b37578cca583006a903382d61cc4451ef77d2
    Author: zhanheng <zhanheng@agora.io>
    Date:   Tue Feb 14 12:15:45 2023 +0800
    
        fix: update submodule
    
    commit 7c437485676f078f69234c207f330cf7b8d92fcb
    Author: zhanheng <zhanheng@agora.io>
    Date:   Tue Feb 14 11:59:46 2023 +0800
    
        fix: join room no auth
    
    commit b092feac7399600b7e74a1766db0592c981b0d3f
    Author: zhanheng <zhanheng@agora.io>
    Date:   Fri Feb 10 12:16:17 2023 +0800
    
        feat: update submodule
    
    commit 9f8b25cc68814d28f371dd668fe6187c528f3739
    Author: zhanheng <zhanheng@agora.io>
    Date:   Wed Feb 8 20:40:46 2023 +0800
    
        feat: update submodule
    
    commit 8d9467ea9d85ecf4c3398c3fd35530b0e654b350
    Author: zhanheng <zhanheng@agora.io>
    Date:   Wed Feb 8 18:57:35 2023 +0800
    
        fix: pr comment
    
    commit 88eb5e4870e665dc3fab91ea7b0aa230e2bd6b63
    Author: zhanheng <zhanheng@agora.io>
    Date:   Tue Feb 7 22:35:26 2023 +0800
    
        feat: update submodule
    
    commit d778cd7306c902fb4ca44198ed23341888f22c84
    Author: zhanheng <zhanheng@agora.io>
    Date:   Tue Feb 7 22:32:12 2023 +0800
    
        fix: postcss config
    
    commit a459d54f242c3aa6c4f06a8dee04b2d9e13cade7
    Author: zhanheng <zhanheng@agora.io>
    Date:   Tue Feb 7 22:11:35 2023 +0800
    
        feat: big class mobile
    
    commit 8ded191fca78bc12e1184227755ccb59b02e4306
    Author: zhanheng <zhanheng@agora.io>
    Date:   Fri Feb 3 12:08:39 2023 +0800
    
        feat: big class h5
    
    commit b767359d59ae025af6ebb92d5baf7222c8d034c0
    Author: zhanheng <zhanheng@agora.io>
    Date:   Wed Feb 1 19:03:34 2023 +0800
    
        feat: postcss landspace config
    
    commit 30ba60541060c02a07f7422fc175f09c26b70685
    Author: zhanheng <zhanheng@agora.io>
    Date:   Thu Jan 12 17:23:52 2023 +0800
    
        chore: update submodule
    
    commit b096af87f71ae721fe27e30f027972a8a0824c7f
    Author: zhanheng <zhanheng@agora.io>
    Date:   Wed Jan 11 10:45:48 2023 +0800
    
        fix: px to vw
    
    commit 106d1431b7c8994f140d77f4a40d319cb5f37b19
    Merge: 8d22ad5 e40148b
    Author: zhanheng <zhanheng@agora.io>
    Date:   Fri Jan 6 17:43:02 2023 +0800
    
        fix:resolve conflicts
    
    commit 8d22ad5a6fc908c24ce5163fd7b0a6f90ad91093
    Author: zhanheng <zhanheng@agora.io>
    Date:   Fri Jan 6 17:41:53 2023 +0800
    
        fix: update version
    
    commit e40148b65d57aef19316185a97226810b7632f56
    Author: 任一飞 <renyifei@agora.io>
    Date:   Fri Jan 6 09:07:19 2023 +0000
    
        Merge pull request #59 in ADUC/open-flexible-classroom-desktop from fix/update-version to release/2.8.11
    
        Squashed commit of the following:
    
        commit f98ac0e085d6d4e418213cd0aa644784e733a739
        Author: zhanheng <zhanheng@agora.io>
        Date:   Fri Jan 6 17:05:46 2023 +0800
    
            fix: update version
    
    commit f98ac0e085d6d4e418213cd0aa644784e733a739
    Author: zhanheng <zhanheng@agora.io>
    Date:   Fri Jan 6 17:05:46 2023 +0800
    
        fix: update version

--- packages/agora-demo-app/postcss.config.js
+++ packages/agora-demo-app/postcss.config.js
@@ -6,7 +6,24 @@ const autoprefixer = require('autoprefixer');
 const tailwindcss = require('tailwindcss');
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const tailwindConfig = require('./tailwind.config');
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const postcssPxToViewport = require('../agora-classroom-sdk/webpack/postcss-plugin/px-to-vw');
 
 module.exports = {
-  plugins: [autoprefixer(), tailwindcss(tailwindConfig)],
+  plugins: [
+    autoprefixer(),
+    tailwindcss(tailwindConfig),
+    postcssPxToViewport({
+      viewportWidth: 375,
+      unitPrecision: 3,
+      viewportUnit: 'vw',
+      fontViewportUnit: 'vw',
+      include: [/\/mobile\//, /\.mobile\./],
+      exclude: [/\/node_modules\//],
+      landscape: true, // 是否处理横屏情况
+      landscapeUnit: 'vw', // (String) 横屏时使用的单位
+      landscapeWidth: 667, // (Number) 横屏时使用的视口宽度
+      landscapeHeight: 375, // (Number) 横屏时使用的视口宽度
+    }),
+  ],
 };
