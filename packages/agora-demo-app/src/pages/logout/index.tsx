a9664a453f2aabd94bfddfc1b153fe51730ca5a0
湛恒 <zhanheng@agora.io>
2023-02-15 08:22:16 +0000

    Merge pull request #85 in ADUC/open-flexible-classroom-desktop from merge-flex to release/2.8.20
    Squashed commit of the following:
    
    commit 6f54c7cb1266ae92f3cb326a774f53bbfa21e742
    Author: renyifei <renyifei@agora.io>
    Date:   Wed Feb 15 16:21:54 2023 +0800
    
        chore: update version
    
    commit 116834910ef91e87bb961e2efff6ecb252165d8e
    Author: renyifei <renyifei@agora.io>
    Date:   Wed Feb 15 16:19:18 2023 +0800
    
        chore: merge
    
    commit 0fac8f4ae17df64ac5490bc73c878d0f958fe992
    Author: renyifei <renyifei@agora.io>
    Date:   Wed Feb 15 16:14:49 2023 +0800
    
        chore: merge flex

--- packages/agora-demo-app/src/pages/logout/index.tsx
+++ packages/agora-demo-app/src/pages/logout/index.tsx
@@ -1,35 +1,62 @@
 import { UserApi } from '@app/api';
 import { SSOLogout } from '@app/components/sso-logout';
-import { RoomStoreContext, UserStoreContext } from '@app/stores';
+import { GlobalStoreContext, RoomStoreContext, UserStoreContext } from '@app/stores';
 import { indexUrl, token } from '@app/utils';
 import { AgoraRteEngineConfig, AgoraRteRuntimePlatform } from 'agora-rte-sdk';
-import { useContext, useLayoutEffect } from 'react';
+import { FC, useContext, useLayoutEffect } from 'react';
 import { useHistory } from 'react-router';
 
 export const Logout = () => {
+  const { setLoading } = useContext(GlobalStoreContext);
   const { setLogin, clearUserInfo } = useContext(UserStoreContext);
   const { clearRooms } = useContext(RoomStoreContext);
-  const history = useHistory();
-  const url = `https://sso2.agora.io/api/v0/logout?redirect_uri=${indexUrl}`;
 
-  useLayoutEffect(() => {
-    if (AgoraRteEngineConfig.platform !== AgoraRteRuntimePlatform.Electron) {
-      logoutWeb();
-    }
-  }, []);
-  const logoutWeb = () => {
-    logout();
-    window.location.href = url;
-  };
-  const logout = () => {
-    UserApi.shared.logoutAccount();
+  const resetLoginState = () => {
     token.clear();
     clearRooms();
     clearUserInfo();
     setLogin(false);
   };
-  const logoutElectron = () => {
-    logout();
+
+  const logout = () => {
+    setLoading(true);
+    return UserApi.shared
+      .logoutAccount()
+      .then(resetLoginState)
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  return AgoraRteEngineConfig.platform !== AgoraRteRuntimePlatform.Electron ? (
+    <LogoutWeb logout={logout} />
+  ) : (
+    <LogoutElectron logout={logout} />
+  );
+};
+
+type Props = {
+  logout: () => Promise<void>;
+};
+
+const LogoutWeb: FC<Props> = ({ logout }) => {
+  const url = `https://sso2.agora.io/api/v0/logout?redirect_uri=${indexUrl}`;
+
+  useLayoutEffect(() => {
+    logout().then(() => {
+      window.location.href = url;
+    });
+  }, []);
+
+  return null;
+};
+
+const LogoutElectron: FC<Props> = ({ logout }) => {
+  const history = useHistory();
+  const url = `https://sso2.agora.io/api/v0/logout`;
+
+  const logoutElectron = async () => {
+    await logout();
     history.replace('/');
   };
 
