24288e4e3509b5025ca7119e5731d319541d2b71
renyifei <renyifei@agora.io>
2023-04-08 19:12:42 +0800

    fix: update sdk.json

--- packages/agora-demo-app/sdk.config.js
+++ packages/agora-demo-app/sdk.config.js
@@ -18,26 +18,22 @@ module.exports = [
   {
     name: 'proctor',
     webpackConfig: 'agora-proctor-sdk/webpack/webpack.dev.js',
-    sources: [
-      {
-        'agora-rte-sdk': 'src',
-        'agora-edu-core': 'src',
-        'agora-common-libs/lib': 'src',
-        'agora-proctor-sdk': 'src/infra/api',
-      },
-    ],
+    sources: {
+      'agora-rte-sdk': 'src',
+      'agora-edu-core': 'src',
+      'agora-common-libs/lib': 'src',
+      'agora-proctor-sdk': 'src/infra/api',
+    },
   },
   {
     name: 'onlineclass',
     webpackConfig: 'agora-onlineclass-sdk/webpack/webpack.dev.js',
-    sources: [
-      {
-        'agora-rte-sdk': 'src',
-        'agora-edu-core': 'src',
-        'agora-common-libs/lib': 'src',
-        'agora-onlineclass-sdk': 'src',
-      },
-    ],
+    sources: {
+      'agora-rte-sdk': 'src',
+      'agora-edu-core': 'src',
+      'agora-common-libs/lib': 'src',
+      'agora-onlineclass-sdk': 'src',
+    },
     assetsDir: 'agora-onlineclass-sdk/src/resources',
   },
 ];
