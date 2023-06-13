8e331533e21d6c37be51e0ebe11c4845986ce88d
renyifei <renyifei@agora.io>
2023-06-13 20:23:54 +0800

    fix: add ci:build

--- ci/build/build_mac.sh
+++ ci/build/build_mac.sh
@@ -1,7 +1,9 @@
-# force_rebuild=1
 source_root=$(pwd)
-build_branch=open_flexible_classroom_desktop_branch
-build_env=
+ci_source_root=$(realpath $source_root/../apaas-cicd-web)
+
+build_branch=$open_flexible_classroom_desktop_branch
+build_env=$env
+
 ci_script_version=v1
 lib_dependencies=(
     agora-rte-sdk
@@ -25,7 +27,7 @@ recording_templates=(
 
 check_dependencies $source_root $build_branch "${lib_dependencies[*]}"
 
-build_demo $source_root
+build_demo $source_root $ci_source_root $build_env
 
 #publish_web $source_root $build_branch $build_env
 
