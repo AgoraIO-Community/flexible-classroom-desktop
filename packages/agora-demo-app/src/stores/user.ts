5e9237269f37d8233523e9f22ca1a529db74c94f
湛恒 <zhanheng@agora.io>
2023-06-20 03:14:39 +0000

    Merge pull request #153 in ADUC/open-flexible-classroom-desktop from fix/demo-display-name to release/2.8.40
    Squashed commit of the following:
    
    commit 05d42bb16cdcaea277400327a0f202ff6c788a77
    Author: renyifei <renyifei@agora.io>
    Date:   Tue Jun 20 11:12:35 2023 +0800
    
        fix: display name

--- packages/agora-demo-app/src/stores/user.ts
+++ packages/agora-demo-app/src/stores/user.ts
@@ -34,13 +34,13 @@ export class UserStore {
   public userInfo: UserInfo | null = getLSStore<UserInfo>(LS_USER_INFO)! || {};
 
   @observable
-  public nickName = getLSStore<string>(LS_NICK_NAME) || this.userInfo?.companyName || '';
+  public nickName = getLSStore<string>(LS_NICK_NAME) || this.userInfo?.displayName || '';
 
   @action.bound
   private setUserInfo(data: UserInfo | null) {
     this.userInfo = data;
-    if (this.nickName === '' && this.userInfo?.companyName) {
-      this.setNickName(this.userInfo.companyName);
+    if (this.nickName === '' && this.userInfo?.displayName) {
+      this.setNickName(this.userInfo.displayName);
     }
   }
 
