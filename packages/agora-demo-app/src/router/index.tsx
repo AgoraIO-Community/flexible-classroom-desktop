f17a43fcd1af267e8848411a924471fd3acf89a9
湛恒 <zhanheng@agora.io>
2023-09-25 12:50:24 +0000

    Merge pull request #241 in ADUC/open-flexible-classroom-desktop from fix/mobile-route to release/2.9.0
    Squashed commit of the following:
    
    commit 94e46de80ccecfd4a593a6fa9df516af5554381e
    Author: renyifei <renyifei@agora.io>
    Date:   Mon Sep 25 20:47:25 2023 +0800
    
        fix: mobile route

--- packages/agora-demo-app/src/router/index.tsx
+++ packages/agora-demo-app/src/router/index.tsx
@@ -1,5 +1,5 @@
-import { useContext, useMemo } from 'react';
-import { Route, Switch, useHistory } from 'react-router';
+import React, { FC, PropsWithChildren, useContext, useMemo } from 'react';
+import { Route, Switch, useHistory, useLocation } from 'react-router';
 import { HashRouter } from 'react-router-dom';
 import { AuthLayout } from '../layout/auth-layout';
 import { BasicLayout } from '../layout/basic-layout';
@@ -9,32 +9,16 @@ import { isH5Browser } from '@app/utils/browser';
 import { GlobalStoreContext } from '@app/stores';
 
 export const RouteContainer = () => {
-  const history = useHistory();
   const globalStore = useContext(GlobalStoreContext);
   const browserPlatformRedirectPaths = useMemo(() => {
     const list = [
+      PageRouter.Index,
       PageRouter.IndexMobileWeb,
       PageRouter.InviteMobileWeb,
       PageRouter.JoinRoomMobileWeb,
     ];
     return list.map((v) => routesMap[v].path);
   }, []);
-
-  if (browserPlatformRedirectPaths.includes(location.pathname)) {
-    const isH5 = isH5Browser();
-    // redirect to mobile web page
-    if (isH5 && !location.pathname.match('/mobile')) {
-      const url = window.location.hash.replace('#/', '/mobile/');
-      history.replace(url);
-      return null;
-    }
-    // redirect to desktop web page
-    if (!isH5 && location.pathname.match('/mobile')) {
-      const url = window.location.hash.replace('#/mobile', '');
-      history.replace(url);
-      return null;
-    }
-  }
 
   const authIncludes = useMemo(() => {
     const list = [PageRouter.CreateRoom, PageRouter.Detail];
@@ -46,23 +30,52 @@ export const RouteContainer = () => {
 
   return (
     <HashRouter>
-      <BasicLayout>
-        <AuthLayout includes={authIncludes} platformRedirectPaths={browserPlatformRedirectPaths}>
-          <Switch>
-            {Object.keys(commonRoutesMap).map((item, index) => {
-              const route = routesMap[item];
-              return (
-                <Route
-                  key={item + index}
-                  exact={!!route.exact}
-                  path={route.path}
-                  component={route.component}
-                />
-              );
-            })}
-          </Switch>
-        </AuthLayout>
-      </BasicLayout>
+      <PlatformRedirect browserPlatformRedirectPaths={browserPlatformRedirectPaths}>
+        <BasicLayout>
+          <AuthLayout includes={authIncludes} platformRedirectPaths={browserPlatformRedirectPaths}>
+            <Switch>
+              {Object.keys(commonRoutesMap).map((item, index) => {
+                const route = routesMap[item];
+                return (
+                  <Route
+                    key={item + index}
+                    exact={!!route.exact}
+                    path={route.path}
+                    component={route.component}
+                  />
+                );
+              })}
+            </Switch>
+          </AuthLayout>
+        </BasicLayout>
+      </PlatformRedirect>
     </HashRouter>
   );
+};
+
+const PlatformRedirect: FC<PropsWithChildren<{ browserPlatformRedirectPaths: string[] }>> = ({
+  children,
+  browserPlatformRedirectPaths,
+}) => {
+  const history = useHistory();
+  const location = useLocation();
+  if (browserPlatformRedirectPaths.includes(location.pathname)) {
+    const isH5 = isH5Browser();
+    // redirect to mobile web page
+    if (isH5 && !location.pathname.match('/mobile')) {
+      const url = window.location.hash.replace('#/', '/mobile/');
+
+      history.push(url);
+      return null;
+    }
+    // redirect to desktop web page
+    if (!isH5 && location.pathname.match('/mobile')) {
+      const url = window.location.hash.replace('#/mobile', '');
+
+      history.push(url);
+      return null;
+    }
+  }
+
+  return <React.Fragment>{children}</React.Fragment>;
 };
