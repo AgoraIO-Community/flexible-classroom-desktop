7556f2e8712d82c055e51d26b11bbf13574c2eb5
任一飞 <renyifei@agora.io>
2023-10-13 03:53:48 +0000

    Merge pull request #261 in ADUC/open-flexible-classroom-desktop from fix/2.9-i18n to release/2.9.1
    Squashed commit of the following:
    
    commit eccf3112112b6a5002904509492de93d5938547b
    Author: zhanheng <zhanheng@agora.io>
    Date:   Fri Oct 13 11:49:20 2023 +0800
    
        fix: logo text

--- packages/agora-demo-app/src/layout/home-layout/index.tsx
+++ packages/agora-demo-app/src/layout/home-layout/index.tsx
@@ -2,16 +2,23 @@ import { observer } from 'mobx-react';
 import { FC, PropsWithChildren, useContext } from 'react';
 import './index.css';
 import { GlobalStoreContext } from '@app/stores';
+import { useI18n } from 'agora-common-libs';
 
 export const HomeLayout: FC<PropsWithChildren<unknown>> = observer(({ children }) => {
   const homeStore = useContext(GlobalStoreContext);
+  const t = useI18n();
 
   return (
     <div className="fcr-w-full fcr-h-full fcr-overflow-auto">
       <div className={`home`}>
         <div className={`home-left ${homeStore.language}`}>
           <header className="fcr-flex fcr-items-center">
-            <div className={`logo ${homeStore.language}`}></div>
+            <div className="fcr-quick-header">
+              <div className="fcr-logo">
+                <img src={require('../../assets/favicon.png')} width={32} height={32} />
+                {t('fcr_feedback_label_fcr')}
+              </div>
+            </div>
           </header>
         </div>
         <div className="home-right fcr-flex-1">{children}</div>
