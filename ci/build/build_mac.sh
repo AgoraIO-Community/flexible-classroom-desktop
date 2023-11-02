0795f650ecd32278835549030ae669a325071c98
任一飞 <renyifei@agora.io>
2023-11-02 06:24:13 +0000

    Merge pull request #273 in ADUC/open-flexible-classroom-desktop from feat/update-2.9.2 to release/2.9.2
    Squashed commit of the following:
    
    commit 2a1f42f248fa90f4bb0c1905a4d804534a7e8183
    Author: zhanheng <zhanheng@agora.io>
    Date:   Thu Nov 2 14:23:28 2023 +0800
    
        fix: update submodule

--- ci/build/build_mac.sh
+++ ci/build/build_mac.sh
@@ -16,8 +16,8 @@ lib_dependencies=(
 )
 lib_versions=(
     2.9.2
-    2.9.1
-    2.9.3
+    2.9.2
+    2.9.4
     1.0.0
     2.9.2
     1.0.6
@@ -25,8 +25,8 @@ lib_versions=(
 )
 lib_branches=(
     release/2.9.2
-    release/2.9.1
-    release/2.9.3
+    release/2.9.2
+    release/2.9.4
     release/2.9.1
     release/2.9.2
     release/1.0.6
