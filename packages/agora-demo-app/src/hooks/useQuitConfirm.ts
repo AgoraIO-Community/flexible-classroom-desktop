b10a197fedd0f952376525c255009a3b35ee2c08
任一飞 <renyifei@agora.io>
2024-01-30 03:36:58 +0000

    Merge pull request #326 in ADUC/open-flexible-classroom-desktop from jira/APAAS-5224 to release/2.9.40
    Squashed commit of the following:
    
    commit 89a26342c151ef5442f0c0dc1e6ca2e14d6f6139
    Author: zhanheng <zhanheng@agora.io>
    Date:   Mon Jan 29 22:16:11 2024 +0800
    
        fix: [APAAS-5224]

--- packages/agora-demo-app/src/hooks/useQuitConfirm.ts
+++ packages/agora-demo-app/src/hooks/useQuitConfirm.ts
@@ -9,6 +9,11 @@ export const useQuitConfirm = () => {
   const transI18n = useI18n();
 
   useEffect(() => {
+    const isSafari = navigator.userAgent.indexOf('Safari') > -1;
+    const isIos = /iPad|iPhone|iPod/.test(navigator.userAgent);
+    if (isIos && isSafari) {
+      return;
+    }
     const unregister = history.block(() => {
       if (!window.confirm(transI18n('fcr_room_tips_leave_room'))) {
         history.goForward();
