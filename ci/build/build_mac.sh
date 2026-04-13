a3384d8172cdc4c3c39f1fd8efc5f7e50555d3ad
renyifei <renyifei@agora.io>
2026-04-13 11:17:07 +0800

    [CHORE]: [CSD-77584] update lib dependencies

--- ci/build/build_mac.sh
+++ ci/build/build_mac.sh
@@ -28,21 +28,21 @@ lib_dependencies=(
     fcr-ui-scene
 )
 lib_versions=(
-    2.9.48
-    2.9.46
+    2.9.50
+    2.9.50
     2.9.41
     2.9.42
-    2.9.40
-    1.0.40
+    2.9.50
+    1.0.50
     1.0.41
 )
 lib_branches=(
-    release/2.9.48
-    release/2.9.46
+    release/2.9.50
+    release/2.9.50
     release/2.9.41
     release/2.9.42
-    release/2.9.40
-    release/1.0.40
+    release/2.9.50
+    release/1.0.50
     release/1.0.41
 )
 
